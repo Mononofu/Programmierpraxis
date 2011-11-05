0c04ddc2b105c02c461a9789a42f9e76194d82e0
Julian Schrittwieser <J.Schrittwieser@gmail.com>
2011-11-05 22:25:06 +0100

    added test script

---
+++ Mustertest/impl/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export PROGRAM_NAME="Scrabble"
+export INPUT_NAME_TEMPLATE="../io/spec.i"
+export OUTPUT_NAME_TEMPLATE="../io/spec.o"
+export GENERATED_OUTPUT_NAME="../io/spec.out"
+export NUMBER_OF_INPUT_FILES="4"
+
+../../test.sh
